98b0abc7006cc619da05520b162f554181d2b60f
IndimaDias <indimad@yahoo.com>
2019-06-06 22:53:26 -0400

    Modified word.js to print the word to a string

--- Word.js
+++ Word.js
@@ -11,18 +11,42 @@ var Letter = require('./Letter.js');
 function Word(wordArray){
     this.wordArray = wordArray;
     // this.
+    this.toString = function(){
+        var checkArray = this.wordArray;
+        var strArray = [];
+
+        for(var i=0; i<checkArray.length; i++){
+            if(checkArray[i] === '\xA0'){
+                strArray.push(" "); 
+              
+            }
+            else{ 
+                strArray.push(checkArray[i].validateLetter());  
+            }
+             
+            
+
+        }
+        console.log(strArray.join(" "));
+    }
 }
 
 var wordRandom = "test test";
 var newWord = [];
 for(var i =0 ; i<wordRandom.length ; i++){
-    
-    newWord.push(new Letter(wordRandom[i]));
+    if(wordRandom[i] === " "){
+        newWord.push('\xA0');
+    }
+    else {
+        newWord.push(new Letter(wordRandom[i]));
+    }    
     
 }
 
 var wordArr = new Word (newWord);
+wordArr.toString();
+
 // wordArr.wordArray - new Word(newWord);
 
-console.log(wordArr);
+// console.log(wordArr);
     
