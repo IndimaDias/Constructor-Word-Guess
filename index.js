3f155afe21afd316506ede651bab5fac0b575648
IndimaDias <indimad@yahoo.com>
2019-06-08 23:44:01 -0400

    index file modified to prompt player to guess a letter

--- index.js
+++ index.js
@@ -3,16 +3,53 @@ var inquire = require('inquirer');
 var Word = require("./Word");
 
 // Generate a random word using random-words npm 
-var newWord = randomWord();
-var wordArray = [];
+var newWord = "";
 var wordObj ;
+var totalGuesses = 10;
 
-function generateArray (randowChiose){  
-    
+// ***********************************function to start the game***************************************
+function start(){
+    inquire.prompt({
+        type : 'list',
+        name : 'userChoise',
+        message : 'Do you want to play?',
+        choices : ["YES","NO"]
+    }).then(answers =>{
+       if(answers.userChoise === 'YES'){
+           generateWord();
+           promptForLetter();
+       }
+    })
+}// end start ******************************************************************************************
+
+// ********************************function to generate a random word **********************************
+
+function generateWord (){  
+    // generate new word
+    newWord = randomWord();
+    console.log(newWord);
+    // make a word object with the generated word
     wordObj = new Word(newWord);
-    // wordObj.toString();
+    // print word 
+    wordObj.toString();
 
+}// end generateWord************************************************************************************
+
+// *******************************function prompt to enter a letter ************************************
+function promptForLetter(){
+    inquire.prompt({
+        type : 'input',
+        name : 'letterGuessed',
+        message : 'You have ' + totalGuesses + ' remaining. Enter a Letter'
+    }).then(answers =>{
+       wordObj.chekLetter(answers.letterGuessed);
+       wordObj.toString();
+});
 }
-console.log(newWord);
-generateArray(newWord);
-console.log(wordObj);
+
+console.log("****************Play Guess the word**********************\n\n");
+
+
+start();
+// generateArray(newWord);
+// console.log(wordObj);
